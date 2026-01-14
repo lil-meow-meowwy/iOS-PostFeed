92c4a9fc75c0f41a66dea13d5a01d1fef2b610b5
Kristina Yaroshenko <91286203+lil-meow-meowwy@users.noreply.github.com>
2026-01-14 12:50:35 +0200

    Initial Commit

---
+++ iOS-PostFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-PostFeed
+//
+//  Created by Kristina Yaroshenko on 14.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
